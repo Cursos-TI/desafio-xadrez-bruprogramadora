123eedd4a4b384d6ab8d6ff9727785374c1a10de
bruprogramadora <sbruna24@gmail.com>
2025-04-30 00:43:47 +0000

    commit

--- xadrez.c
+++ xadrez.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
-int main () {
-    int i; 
-    int j;
-    int k;
 
-    printf (" Movimento da torre: \n");
-    for ( i =1 ; i <= 5; i++) {
-        printf (" Direita \n");
+int main() {
+    int i, j, k;
+
+    // Movimento da Torre
+    printf("Movimento da Torre:\n");
+    for (i = 1; i <= 5; i++) {
+        printf("Direita\n");
     }
-    printf ("\n Movimento do bispo :\n");
-     j =1;
-    while ( j <= 5){
-        printf (" Cima direita\n");
+
+    // Movimento do Bispo
+    printf("\nMovimento do Bispo:\n");
+    j = 1;
+    while (j <= 5) {
+        printf("Cima direita\n");
         j++;
     }
-printf (" \n Movimento da rainha:\n");
-k=1;
 
-do {
-    printf ("Esquerda\n");
-    k++;
-} while (k <= 8);
+    // Movimento da Rainha
+    printf("\nMovimento da Rainha:\n");
+    k = 1;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k <= 8);
 
+    // Movimento do Cavalo
+    printf("\nMovimento do Cavalo:\n");
+
+    // Define quantas vezes o cavalo fará o movimento completo em "L"
+    int movimentos = 3; // por exemplo, 3 movimentos em "L"
+    int m, n;
+
+    for (m = 1; m <= movimentos; m++) {
+        // Move duas casas para baixo
+        n = 1;
+        while (n <= 2) {
+            printf("Baixo\n");
+            n++;
+        }
+        // Move uma casa para a esquerda
+        printf("Esquerda\n");
+    }
 
     return 0;
 }
